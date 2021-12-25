6f4d76fbbdb45896529c088932c19604d181a6bf
rajtarovN <68690942+rajtarovN@users.noreply.github.com>
2021-12-25 19:21:16 +0100

    started bartender

---
+++ restaurant-front/src/modules/bartender/pages/add-drink/add-drink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-drink',
+  templateUrl: './add-drink.component.html',
+  styleUrls: ['./add-drink.component.scss']
+})
+export class AddDrinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
