b1fadf3d1cf18f42ef379b01696c0e29f707c21e
rajtarovN <68690942+rajtarovN@users.noreply.github.com>
2022-01-10 20:54:08 +0100

    update

--- restaurant-front/src/modules/shared/shared.module.ts
+++ restaurant-front/src/modules/shared/shared.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InterceptorInterceptor } from './interceptors/interceptor.interceptor';
+import { DrinkCardComponent } from './components/drink-card/drink-card.component';
+import { OneDrinkCardComponent } from './components/one-drink-card/one-drink-card.component';
+import { OrderViewComponent } from './components/order-view/order-view.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DrinkCardComponent, OneDrinkCardComponent, OrderViewComponent],
   imports: [
     CommonModule
   ],
