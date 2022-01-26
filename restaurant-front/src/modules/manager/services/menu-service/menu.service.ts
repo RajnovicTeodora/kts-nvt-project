90bc582524d42ac9e7699c584a75f78cadebbb23
Teodora Rajnovic <rajnovicteodora@gmail.com>
2022-01-26 11:50:50 +0100

    Update menu.service.ts

--- restaurant-front/src/modules/manager/services/menu-service/menu.service.ts
+++ restaurant-front/src/modules/manager/services/menu-service/menu.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UpdateMenuItemPrice } from 'src/modules/shared/models/menu-models/update-menu-item-price';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +10,48 @@ export class MenuService {
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) {}
-  getAll(): Observable<any> {
+  getAll(name: string): Observable<any> {
+    let param = { searchName: name };
+
     let queryParams = {};
 
     queryParams = {
       headers: this.headers,
       observe: 'response',
+      params: param,
     };
 
-    return this.http.get('http://localhost:8080/api/menuItem/getAll'); // TODO
+    return this.http.get('http://localhost:8080/api/menu/getAll', queryParams);
+  }
+
+  deleteMenuItem(id: string): Observable<any> {
+    return this.http.delete(
+      'http://localhost:8080/api/menu/deleteMenuItem/' + id,
+      { headers: this.headers, responseType: 'json' }
+    );
+  }
+
+  toggleIsMenuItemActive(id: string): Observable<any> {
+    let param = { id: id };
+    return this.http.put(
+      'http://localhost:8080/api/menu/toggleIsActive',
+      {},
+      {
+        headers: this.headers,
+        observe: 'response',
+        params: param,
+      }
+    );
+  }
+
+  updateMenuItemPrice(data: UpdateMenuItemPrice): Observable<any> {
+    return this.http.post<UpdateMenuItemPrice>(
+      'http://localhost:8080/api/menuItemPrice/updatePrice',
+      data,
+      {
+        headers: this.headers,
+        responseType: 'json',
+      }
+    );
   }
 }
