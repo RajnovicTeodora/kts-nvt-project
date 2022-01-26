cc82c5a1c583cd9cce288dde1d863f22e00f7bbf
Isidora <69584865+isidorasavic@users.noreply.github.com>
2022-01-26 01:41:12 +0100

    Changed that deleted users can't login

--- restaurant-back/src/main/java/com/ftn/restaurant/repository/UserRepository.java
+++ restaurant-back/src/main/java/com/ftn/restaurant/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ftn.restaurant.model.User;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,6 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     public Page<User> findAll(Pageable pageable);
 
     Optional<User> findByUsername(String username);
+
+    @Query("select u from User u where " + 
+        "u.username = :username and u.deleted = false")
+    Optional<User> findByUsernameAndNotDeleted(@Param("username") String username);
 
     @Query("select user.loggedFirstTime from User user where user.username =?1")
     boolean findIsLoggedInFirstTimeByUsername(String username);
