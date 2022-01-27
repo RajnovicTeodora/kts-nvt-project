3abe46ac7f0978aa5ac21ae56c191dd424d12ff3
Teodora Rajnovic <rajnovicteodora@gmail.com>
2022-01-27 14:26:29 +0100

    Item view page test

--- restaurant-back/src/test/java/com/ftn/restaurant/e2e/pages/ManagerDashboardPage.java
+++ restaurant-back/src/test/java/com/ftn/restaurant/e2e/pages/ManagerDashboardPage.java
@@ -11,32 +11,48 @@ public class ManagerDashboardPage {
 
     private static final String URL = "http://localhost:4200/manager-dashboard";
 
-    @FindBy(xpath="//mat-toolbar//button")
+    @FindBy(xpath = "//mat-toolbar//button")
     private WebElement menuButton;
 
     // Side menu buttons
-    @FindBy(xpath="//mat-list-item")
+    @FindBy(xpath = "//mat-list-item")
     private List<WebElement> listItemButtons;
+
+    @FindBy(id = "logout-button")
+    private WebElement logoutButton;
+
+    @FindBy(id = "confirm-logout-button")
+    private WebElement confirmLogoutButton;
 
     public ManagerDashboardPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public boolean urlPresent(){ return Utilities.urlWait(driver, URL, 10);}
+    public boolean urlPresent() {
+        return Utilities.urlWait(driver, URL, 10);
+    }
 
-    public void itemsViewBtnClick(){
+    public void itemsViewBtnClick() {
         Utilities.clickableWait(driver, this.listItemButtons.get(0), 10).click();
     }
 
-    public void menuViewBtnClick(){
+    public void menuViewBtnClick() {
         Utilities.clickableWait(driver, this.listItemButtons.get(1), 10).click();
     }
 
-    public void paychecksViewBtnClick(){
+    public void paychecksViewBtnClick() {
         Utilities.clickableWait(driver, this.listItemButtons.get(2), 10).click();
     }
 
-    public void reportsViewBtnClick(){
+    public void reportsViewBtnClick() {
         Utilities.clickableWait(driver, this.listItemButtons.get(3), 10).click();
+    }
+
+    public void logoutBtnClick() {
+        Utilities.clickableWait(driver, this.logoutButton, 10).click();
+    }
+
+    public void confirmLogoutBtnClick() {
+        Utilities.clickableWait(driver, this.confirmLogoutButton, 10).click();
     }
 }
